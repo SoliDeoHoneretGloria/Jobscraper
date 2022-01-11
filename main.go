d932c1c7663d6943762fcbc341749a414621d656
SoliDeoHoneretGloria <iphone_1st@naver.com>
2022-01-11 18:50:05 +0100

    v1.04 extractJob added

--- main.go
+++ main.go
@@ -5,9 +5,19 @@ import (
 	"log"
 	"net/http"
 	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
+
+type extractedJob struct {
+	id       string
+	title    string
+	company  string
+	location string
+	salary   string
+	summary  string
+}
 
 var baseURL string = "https://kr.indeed.com/jobs?q=python&limit=50"
 
@@ -25,6 +35,34 @@ func main() {
 func getPage(page int) {
 	pageURL := baseURL + "&start=" + strconv.Itoa(page*50)
 	fmt.Println("Requesting", pageURL)
+	res, err := http.Get(pageURL)
+	checkErr(err)
+	checkCode(res)
+
+	defer res.Body.Close()
+	doc, err := goquery.NewDocumentFromReader(res.Body)
+	checkErr(err)
+
+	searchCards := doc.Find(".tapItem")
+
+	searchCards.Each(func(i int, card *goquery.Selection) {
+		extractJob(card)
+	})
+}
+
+func extractJob(card *goquery.Selection) {
+	id, _ := card.Attr("data-jk")
+	title := card.Find(".jobTitle>span").Text()
+	name := card.Find("span.companyName").Text()
+	location := card.Find(".companyLocation").Text()
+	salary := card.Find(".salary-snippet").Text()
+	summary := card.Find(".job-snippet").Text()
+	fmt.Println(id, title, name, location, salary, summary)
+
+}
+
+func cleanString(str string) string {
+	return strings.Join(strings.Fields(strings.TrimSpace(str)), " ")
 }
 
 func getPages() int {
